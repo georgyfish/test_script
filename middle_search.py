dbb3092524d2907228a839d075a0e87f809b876d
georgyfish <13056599771@163.com>
2024-03-24 18:52:33 +0800

    # 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交# 说明将会终止提交。## 位于分支 main# 您的分支与上游分支 'origin/main' 一致。## 要提交的变更：#	修改：     middle_search.py## 尚未暂存以备提交的变更：#	修改：     commit.sh## 未跟踪的文件:#	1.sh#	__pycache__/#	deb.sh#	get_deb_test_result.py#	maopao.py#	middle_chazhao.py#	ping.txt#	"print(\"hell, world!\").py"#	test.py#	try_reboot.sh#	xc_run_heaven_loop.sh#	xc_run_heaven_valley_perf(2).py#	xc_tool/#

--- middle_search.py
+++ middle_search.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+# import get_commit
+# import get_deb_test_result
 
-lis1 = ["commit0","commit1","commit2","commit3","commit4","commit5","commit6","commit7","commit8","commit9","commit10","commit11"]
-dic1 = {"commit0":"true","commit1":"true","commit2":"true","commit3":"true","commit4":"true","commit5":"true","commit6":"true","commit7":"true","commit8":"true","commit9":"true","commit10":"true","commit11":"false","commit12":"false"}
+lis1 = ["commit0","commit1","commit2","commit3","commit4","commit5","commit6","commit7","commit8","commit9","commit10","commit11","commit12"]
+dic1 = {"commit0":"true","commit1":"true","commit2":"true","commit3":"true","commit4":"true","commit5":"true","commit6":"true","commit7":"true","commit8":"true","commit9":"true","commit10":"true","commit11":"true","commit12":"true"}
 
-def middle_check(lis1,dic1):
+
+def middle_check(lis1):
     # left、right初始值为列表元素的序号index 最小值和最大值
     left = 0 
     count = 0
+    result = []
     right = len(lis1) - 1
+    # dic1[lis1[left]] = get_deb_test_result(lis1[left])
+    # dic1[lis1[right]] = get_deb_test_result(lis1[right])
+    if dic1[lis1[left]] == dic1[lis1[right]]:
+        print("此区间内，第一个元素和最后一个元素的结果相等，请确认")
+        return -1
     while left <= right -2:
         #中间值为 left+right的和除2，取商
         middle = (left + right)//2 
@@ -17,20 +26,6 @@ def middle_check(lis1,dic1):
         # print(f"dic1[lis1[middle]]={dic1[lis1[middle]]}") #这里写交互或者执行测试得到结果
         # print(f"dic1[lis1[left]]={dic1[lis1[left]]},dic1[lis1[right]]={dic1[lis1[right]]}")
         #循环结束条件：
-        # if   right - left == 2:
-        #     print(f"循环结束")
-        #     if dic1[lis1[middle]] == dic1[lis1[left]]:
-        #         left = middle 
-        #     elif dic1[lis1[middle]] == dic1[lis1[right]]:
-        #         right = middle 
-        #     print(f"定位到引入范围是 {lis1[left]},{lis1[right]}")               
-        #     break
-        # elif dic1[lis1[middle]] == dic1[lis1[left]]:
-        #     left = middle 
-        # elif dic1[lis1[middle]]  == dic1[lis1[right]]:
-        #     right = middle 
-        # else:
-        #     print("pass")
         if dic1[lis1[middle]] == dic1[lis1[left]]:
                 left = middle 
         elif dic1[lis1[middle]] == dic1[lis1[right]]:
@@ -38,7 +33,10 @@ def middle_check(lis1,dic1):
         # print(f"定位到引入范围是 {lis1[left]},{lis1[right]}") 
     else:
         print(f"count={count}")
-        print(f"定位到引入范围是 {lis1[left]},{lis1[right]}") 
+        print(f"定位到引入范围是 {lis1[left]}(VNP)-{lis1[right]}(VP)之间引入") 
+        result = [lis1[left],lis1[right]]
+        return result
 
 if __name__ == "__main__":
-    middle_check(lis1,dic1)
+
+    print(middle_check(lis1))
