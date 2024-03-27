a5955a2db36aad261cb0524ebf1566787257c418
georgyfish <13056599771@163.com>
2024-03-28 00:24:23 +0800

    # 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交# 说明将会终止提交。## 位于分支 main# 您的分支与上游分支 'origin/main' 一致。## 要提交的变更：#	修改：     test.py## 尚未暂存以备提交的变更：#	修改：     commit.sh## 未跟踪的文件:#	1.sh#	__pycache__/#	deb.sh#	get_deb_test_result.py#	maopao.py#	middle_chazhao.py#	ping.txt#	"print(\"hell, world!\").py"#	try_reboot.sh#	xc_run_heaven_loop.sh#	xc_run_heaven_valley_perf(2).py#	xc_tool/#

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
+from datetime import datetime, timezone, timedelta
+import middle_search
 
-# 1. 根据时间区间拿到deb区间，
+# 1. 根据时间区间拿到deb大致区间，
 # 2. 二分法定位  执行deb，拿到测试结果，得到deb引入区间；
 # 3. 根据deb 区间，定位到UMD、KMD commit区间；
 # 4. 定位UMD commit引入，
 # 5. UMD定位不到，执行KMD定位；
+
+def get_deb_section(branch, begin_time,end_time):
+    result = []
+    return result
+
+# 根据deb version拿到umd、kmd区间
+def get_commit(repo, branch,list):
+    result = []
+    return result
+
+
+contribute_deb = middle_search.middle_check(get_deb_section("develop","2024-02-29 00:00:00", "2024-03-01 00:00:00"))
+umd_list = get_commit("gr-umd","develop",contribute_deb)
+middle_search.middle_check(umd_list)
+
+if __name__ == "__main__":
+    pass
