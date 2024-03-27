f8e435f1ca036095753962049e481e82cc7625de
georgyfish <13056599771@163.com>
2024-03-27 20:40:54 +0800

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is up to date with 'origin/main'.## Changes to be committed:#	modified:   get_deb_version.py#

--- get_deb_version.py
+++ get_deb_version.py
@@ -13,36 +13,45 @@ def deal(begin_date, end_date):
         date_str = begin_date.strftime("%Y%m%d")
         date_list.append(date_str)
         begin_date += datetime.timedelta(days=1)
-    print(date_list)
-    return date_list
-
-def get_deb_version(branch,begin_date,end_date):
-    result = []
-    date_list = deal(begin_date, end_date)
+    # print(date_list)
     work_date_list = []
     # begin_time和end_time之间日期的 去除周末的日期
     for i in date_list:
         i = datetime.datetime.strptime(i,"%Y%m%d")
         if i.weekday() < 5 :
             work_date_list.append(i.strftime("%Y%m%d"))
-    swqa_ssh_login_cmd = "sshpass -p gfx123456 ssh swqa@192.168.114.8 -o StrictHostKeyChecking=no"
-    for work_date in work_date_list:
-        rs = subprocess.Popen(f"curl https://oss.mthreads.com/product-release/{branch}/{work_date}/daily_build.txt", shell=True, close_fds=True, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn = os.setsid)
-        for line in rs.stdout.readlines():
-            line = line.decode().strip()
-            line_list = []
-            line_list.append(line)
-        result.append(line_list[0])
-    print(result)
-#         cmd = f'''
-#         {swqa_ssh_login_cmd} "cd /home/swqa/xc_tool/ &&  ./download_driver.py -b {branch} -v {work_date}"
-#         {swqa_ssh_login_cmd} "sudo dpkg -i /home/swqa/xc_tool/driver/"
-
-# '''
-#         os.system(cmd)
-
     print(work_date_list)
+    return work_date_list
+
+def get_deb_version(branch,begin_date,end_date):
+    result = {}
+    work_date_list = deal(begin_date, end_date)
+    
+    for work_date in work_date_list:
+        rs = subprocess.Popen(f"curl https://oss.mthreads.com/product-release/{branch}/{work_date}/daily_build.txt", shell=True, close_fds=True, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn = os.setsid).communicate()
+        # output = subprocess.Popen(f"curl https://oss.mthreads.com/product-release/{branch}/{work_date}/daily_build.txt",stdout=subprocess.PIPE,shell=True).communicate()
+        # print(rs[0])
+        rs =  rs[0].decode().strip()
+        out_list = rs.splitlines()
+        result[work_date] = out_list[0]
+    print(result)
+    return result
+
+#download driver && install 
+def install_driver(branch,begin_date,end_date):
+    driver_lic = get_deb_version(branch,begin_date,end_date)
+    swqa_ssh_login_cmd = "sshpass -p gfx123456 ssh swqa@192.168.114.8 -o StrictHostKeyChecking=no"
+    for work_date,driver_version in driver_lic.items():
+        driver_name = f"{driver_version}+dkms+glvnd-Ubuntu_amd64.deb"
+        driver_url = f"https://oss.mthreads.com/product-release/{branch}/{work_date}/{driver_name}"
+
+        cmd = f'''
+            {swqa_ssh_login_cmd} 'cd /home/swqa/ && wget --no-check-certificate -q {driver_url} '
+        '''
+        os.system(cmd)
+
+
+
 
 if __name__ == '__main__':
-    # deal('20180221', '20180305')
-    get_deb_version('develop','20240301', '20240305')
+    install_driver('develop','20240301', '20240305')
