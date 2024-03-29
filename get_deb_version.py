097a89f9273898f136440745366d745f271d4422
georgyfish <13056599771@163.com>
2024-03-29 18:36:59 +0800

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is up to date with 'origin/main'.## Changes to be committed:#	modified:   get_deb_version.py## Changes not staged for commit:#	modified:   middle_search.py#	modified:   test.py## Untracked files:#	__pycache__/#

--- get_deb_version.py
+++ get_deb_version.py
@@ -45,21 +45,23 @@ def get_deb_version(branch,begin_date,end_date):
     print(result)
     return result
 
+
+# driver_dic = {'20240326': ['musa_2024.03.26-D+10129', 'https://oss.mthreads.com/release-ci/repo_tags/20240326.txt', 'https://oss.mthreads.com/product-release/develop/20240326/musa_2024.03.26-D+10129+dkms+glvnd-Ubuntu_amd64.deb', 'musa_2024.03.26-D+10129+dkms+glvnd-Ubuntu_amd64.deb'], '20240327': ['musa_2024.03.27-D+10151', 'https://oss.mthreads.com/release-ci/repo_tags/20240327.txt', 'https://oss.mthreads.com/product-release/develop/20240327/musa_2024.03.27-D+10151+dkms+glvnd-Ubuntu_amd64.deb', 'musa_2024.03.27-D+10151+dkms+glvnd-Ubuntu_amd64.deb']}
 #download driver && install 
-def install_driver(driver_dic,Test_Host_IP):
+def install_driver(driver_dic,Test_Host_IP,index):
     result = {}
     date_list = list(driver_dic.keys())
     driver_url_list = list(driver_dic.values()) 
     left = 0
     right = len(driver_url_list) - 1
-    
-    for i in range(len(date_list)):
+    while True:
+    #for i in range(len(date_list)):
         swqa_ssh_login_cmd = f"sshpass -p gfx123456 ssh swqa@{Test_Host_IP} -o StrictHostKeyChecking=no"
-        print('=='*10 + f"Download {driver_url_list[i][2]}" + '=='*10)
-        os.system(f"{swqa_ssh_login_cmd} 'cd /home/swqa/ && wget --no-check-certificate -q {driver_url_list[i][2]} -O {driver_url_list[i][-1]}'")
-        print('=='*10 +  f"sudo dpkg -i /home/swqa/{driver_url_list[i][-1]} && sudo reboot" + '=='*10)
-        os.system(f"{swqa_ssh_login_cmd} 'sudo dpkg -i /home/swqa/{driver_url_list[i][-1]} && sudo reboot'")
-        time.sleep(150)
+        print('=='*10 + f"Download {driver_url_list[index][2]}" + '=='*10)
+        os.system(f"{swqa_ssh_login_cmd} 'cd /home/swqa/ && wget --no-check-certificate -q {driver_url_list[index][2]} -O {driver_url_list[index][-1]}'")
+        print('=='*10 +  f"sudo dpkg -i /home/swqa/{driver_url_list[index][-1]} && sudo reboot" + '=='*10)
+        # os.system(f"{swqa_ssh_login_cmd} 'sudo dpkg -i /home/swqa/{driver_url_list[index][-1]} && sudo reboot'")
+        # time.sleep(150)
         try:
             for i in range(3):
                 # time.sleep(10)
@@ -70,9 +72,11 @@ def install_driver(driver_dic,Test_Host_IP):
             print(f"ping {Test_Host_IP} fail!")
             exit()
         # 安装驱动后需手动测试，并输入测试结果：
-        rs = input(f"{driver_url_list[i][-1]}已安装，请执行测试并输入测试结果：")
-        result[date_list[i]] = rs
+        rs = input(f"{driver_url_list[index][-1]}已安装，请执行测试并输入测试结果：")
+        driver_name = driver_url_list[index][0]
+        result[driver_name] = rs
     # return result
+        # {'musa_2024.03.26-D+10129':'Y'}
 
     if result[date_list[left]] == result[date_list[right]]:
         print("此区间内，第一个元素和最后一个元素的结果相等，请确认")
