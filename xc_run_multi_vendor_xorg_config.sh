455af22a741b6989075b942010825bb5ebc9cf8f
georgyfish <13056599771@163.com>
2024-05-23 19:34:10 +0800

    hh# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is up to date with 'origin/main'.## Changes to be committed:#	modified:   xc_run_multi_vendor_xorg_config.sh## Changes not staged for commit:#	deleted:    VulkanDemos.py#	modified:   video.py## Untracked files:#	VulkanDemos-BoblChen.py#

--- xc_run_multi_vendor_xorg_config.sh
+++ xc_run_multi_vendor_xorg_config.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-primary_gpu=$1
-card_cnt=$(lspci |grep -Ei "vga|3D|1ed5|MTT"|grep -v audio|grep -v NVMe|awk '{print $1}'|wc -l)
 function check_para() {
     primary_gpu=$1
-    if [[ $# != 1 ]];then 
+    if [[ $# == 0 ]];then 
+        check_current
+    fi
+    if (( $# -lt 1 ));then 
         echo "请使用合法参数:$0 card0/card1"
         exit 1
     fi
@@ -12,19 +13,19 @@ function check_para() {
     typeset -u name
     name=$primary_gpu
     if [[ ${name:0:4} != "CARD" ]];then
-        echo "请使用合法参数:$0 card0/card1"
-        echo $name
+        echo "切换PrimaryGPU测试请使用合法参数:$0 card0/card1"
+        # echo $name
         exit 1
     fi
     if [[ ${primary_gpu:(-1)} =~ ^[0-9]+$ ]];then
-        primary_gpu="Card${primary_gpu:(-1)}"
+        primary_gpu="card${primary_gpu:(-1)}"
     else
-        echo "请使用合法参数:$0 card0/card1"
+        echo "切换PrimaryGPU测试请使用合法参数:$0 card0/card1"
         exit 1
     fi
-    # 边界值，Card0-n应当小于显卡数量
-    cardID=`echo $primary_gpu |awk -F 'Card' '{print $2}'`
-    if [ $cardID -gt $(expr $card_cnt - 1) ];then 
+    # 边界值，card0-n应当小于显卡数量;显卡数量应大于等于2；
+    cardID=`echo $primary_gpu |awk -F 'card' '{print $2}'`
+    if [[ $cardID -gt $(expr $card_cnt - 1) ]] || (( $card_cnt < 2)) ;then 
         echo "GPU序号错误"
         exit 1
     fi
@@ -39,7 +40,7 @@ function xorg_config() {
     mtgpu_config=""
     layout=""
     layout_config=""
-    for pcie_addr in $(lspci |grep -Ei "vga|3D|1ed5|MTT"|grep -v audio|grep -v NVMe|awk '{print $1}');do
+    for pcie_addr in $(lspci |grep -Ei "VGA compatible controller|3D controller|1ed5|MTT"|grep -v audio|grep -v NVMe|awk '{print $1}');do
         #echo $pcie_addr
         mt_bus_id=$((16#$(echo $pcie_addr|cut -d ':' -f1)))
         bus_id_1=$(echo $pcie_addr|awk -F':' '{print $2}')
@@ -48,7 +49,7 @@ function xorg_config() {
         mt_bus_id="$mt_bus_id:$b:$c"
         #echo $mt_bus_id
         device_id=$(find /sys/devices -name drm|grep $pcie_addr|xargs -n1 ls|grep card|grep -oE '[0-9]+')
-        echo "Card$device_id : $(lspci |grep $pcie_addr)"
+        echo "card$device_id : $(lspci |grep $pcie_addr)"
         driver=$(lspci -k |grep $pcie_addr -A3|grep "driver in use" |awk -F ": " '{print $2}')
         mtgpu_config_base=''
 
@@ -56,27 +57,27 @@ function xorg_config() {
         if [[ $device_id != '' || $device_id != ' ' ]];then 
             mtgpu_config_base="
 Section \"Device\"
-        Identifier      \"Card$device_id\"
+        Identifier      \"card$device_id\"
         Driver          \"$driver\"
         BusID           \"PCI:$mt_bus_id\"
 EndSection
 
 Section \"Screen\"
-        Identifier      \"Card$device_id\"
+        Identifier      \"card$device_id\"
         #Monitor         \"Monitor$device_id\"
-        Device          \"Card$device_id\"
+        Device          \"card$device_id\"
 EndSection
 "
-            if [[ "Card$device_id" != $primary_gpu ]]
+            if [[ "card$device_id" != $primary_gpu ]]
             then 
-                layout="Inactive \"Card$device_id\"
+                layout="Inactive \"card$device_id\"
         "
                 layout_config="$layout_config$layout"
             fi
         fi
         mtgpu_config="$mtgpu_config$mtgpu_config_base"
     done
-    echo $layout_config
+    # echo $layout_config
     layout_config="$layout_config
 EndSection
     "
@@ -97,42 +98,49 @@ EndSection
     echo "$xorg" > ~/xorg.conf
 }
 
+# """
+# 多卡环境下如何检查哪张显卡是primary gpu呢；
+#     查看Xorg日志：
+#     lightdm Xorg log path: "/var/log/Xorg.0.log"
+#     gdm3 Xorg log path: "~/.local/share/xorg/Xorg.0.log"
+# 	MTGPU(0) 对应的card应该才是 primary GPU；如果是集显、A卡、N卡？
+#     MTGPU(G0) 对应的card才是 secondary gpu; 
+# 当前机器上的card0 \card1 分别是哪张显卡呢，输出对应的显卡信息；
+# 当前机器上的primary gpu？
+# """
+
+
+
 function check_primarygpu() {
-    cardID=`echo $primary_gpu |awk -F 'Card' '{print $2}'`
-    for pcie_addr in $(lspci |grep -Ei "vga|3D|1ed5|MTT"|grep -v audio|grep -v NVMe|awk '{print $1}');do
-        #echo $pcie_addr
-        mt_bus_id=$((16#$(echo $pcie_addr|cut -d ':' -f1)))
-        b=$((16#$(echo $pcie_addr|awk -F':' '{print $2}'|awk -F'.' '{print $1}')))
-        c=$((16#$(echo $pcie_addr|awk -F':' '{print $2}'|awk -F'.' '{print $2}')))
-        mt_bus_id="$mt_bus_id:$b:$c"
-        #echo $mt_bus_id
-        device_id=$(find /sys/devices -name drm|grep $pcie_addr|xargs -n1 ls|grep card|grep -oE '[0-9]+')
-        # echo "Card$device_id : $(lspci |grep $pcie_addr)"
-        
-        if [ $device_id -eq $cardID ];then
-            c=$(lspci -nn |grep $pcie_addr|awk -F: '{print $4}')
-            set_primarygpu_deviceID=${c%%]*}
-        fi
-    done
-    current_primarygpu_deviceID=`grep "\<PCI:\*" /var/log/Xorg.0.log |awk  '{print $5}'|awk -F: '{print $2}'`
-    echo "set_primarygpu_deviceID=$set_primarygpu_deviceID"
-    echo "current_primarygpu_deviceID=$current_primarygpu_deviceID"
-    if [ $set_primarygpu_deviceID -ne $current_primarygpu_deviceID ];then 
-        echo "检查deviceID失败，boot_vga=$current_primarygpu_deviceID"
+    # 检查 $primary_gpu 和 $current_PrimaryGPU的值是否相同；
+    current_PrimaryGPU=`grep "(0): using drv /dev/dri/card" /var/log/Xorg.0.log |awk  -F"/dev/dri/" '{print $2}'`
+    secondaryGPU=`grep "(G0): using drv /dev/dri/card" /var/log/Xorg.0.log |awk  -F"/dev/dri/" '{print $2}'`
+
+    if [[ $primary_gpu -eq $current_PrimaryGPU ]];then
+        echo "Set $primary_gpu PrimaryGPU success!"
     else
-        echo "检查deviceID正确, boot_vga=$current_primarygpu_deviceID"
+        echo "Set $primary_gpu PrimaryGPU fail! Current PrimaryGPU is $current_PrimaryGPU"
     fi
 
 }
+function check_current() {
+    current_PrimaryGPU=`grep "(0): using drv /dev/dri/card" /var/log/Xorg.0.log |awk  -F"/dev/dri/" '{print $2}'`
+    secondaryGPU=`grep "(G0): using drv /dev/dri/card" /var/log/Xorg.0.log |awk  -F"/dev/dri/" '{print $2}'`
+    vga_info=$(lspci |grep $(find  /sys/devices -name $current_PrimaryGPU |grep drm |awk -F"/" '{print $(NF-2)}'|awk -F: '{print $2":"$3}'))
+    echo "当前环境PrimaryGPU是$current_PrimaryGPU : $vga_info"
+    echo "当前环境SecondaryGPU是$secondaryGPU : $(lspci |grep $(find  /sys/devices -name $secondaryGPU |grep drm |awk -F"/" '{print $(NF-2)}'|awk -F: '{print $2":"$3}'))"
+}
 
+primary_gpu=$1
+card_cnt=$(lspci |grep -Ei "VGA compatible controller|3D controller|1ed5|MTT"|grep -v audio|grep -v NVMe|awk '{print $1}'|wc -l)
 check_para $primary_gpu
+
 xorg_config
 # sudo systemctl stop lightdm
 # sudo ps -ef |grep Xorg|awk '{print $2}'|xargs -n1 kill -9
-
 sudo cp ~/xorg.conf /etc/X11
 echo "sudo cp ~/xorg.conf /etc/X11"
 echo "sudo systemctl restart lightdm "
 sudo systemctl restart lightdm 
 sleep 5
-# check_primarygpu
+check_primarygpu
